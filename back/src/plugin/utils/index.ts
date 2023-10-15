f41c08bbab5d2b32fef8200d4a7fb4dc9cb72eb5
mznx <mznx.ux@gmail.com>
2023-10-15 23:05:52 +0400

    [B] update utils local plugin

--- back/src/plugin/utils/index.ts
+++ back/src/plugin/utils/index.ts
@@ -2,8 +2,14 @@ import { FastifyInstance } from 'fastify';
 import fastifyPlugin from 'fastify-plugin';
 import * as crypto from './crypto.utils';
 
+const data = {
+    crypto: crypto,
+};
+
+declare module 'fastify' {
+    export interface FastifyInstance { utils: typeof data }
+}
+
 export default fastifyPlugin(async (fastify: FastifyInstance) => {
-    fastify.decorate('utils', {
-        crypto: crypto,
-    });
+    fastify.decorate('utils', data);
 });
--- back/src/plugin/utils/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-import * as crypto from '../crypto.utils';
-
-declare module 'fastify' {
-    export interface FastifyInstance {
-        utils: {
-            crypto: typeof crypto,
-        }
-    }
-}
