0a9cc64dd7bafe9cea6909053e5f98fe678a576e
mznx <mznx.ux@gmail.com>
2023-10-22 11:12:19 +0400

    [B] rename local plugin files

--- back/src/system/pluginLoader.system.ts
+++ back/src/system/pluginLoader.system.ts
@@ -2,9 +2,9 @@ import { FastifyInstance } from 'fastify';
 import fastifyPlugin from 'fastify-plugin';
 import cookie from '@fastify/cookie';
 import jwt from '@fastify/jwt';
-import conf from '../plugin/conf';
-import db from '../plugin/db';
-import utils from '../plugin/utils';
+import conf from '../plugin/conf/conf.plugin';
+import db from '../plugin/db/db.plugin';
+import utils from '../plugin/utils/utils.plugin';
 
 export default fastifyPlugin(async (fastify: FastifyInstance) => {
     await fastify.register(conf);
